392c36ca731d4f5bbceb0004690098463d690cc2
Dmitry Balalin <balalindmitry@gmail.com>
2020-07-22 16:18:47 +0300

    add hamcrest, fix мфкÑvarious things

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -4,29 +4,30 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class TestSuite extends TestConf {
+    static String jsonToken;
+    static String id;
 
-    String jsonToken;
-
-    @Before
-    @Test
-    public void checkPing() {
-        given()
+    @BeforeClass
+    public static void checkPingAndAuthorization() {
+        int statusCode = given()
                 .when()
                 .get(EndPoint.PING)
                 .then()
-                .statusCode(201)
-                .log().all();
-    }
+                .extract().statusCode();
 
-    @Before
-    @Test
-    public void createToken() {
+        assertThat(201, equalTo(statusCode));
 
+        //authorization
         String bodyUserPassword = "{\n" +
                 "    \"username\" : \"admin\",\n" +
                 "    \"password\" : \"password123\"\n" +
@@ -34,28 +35,60 @@ public class TestSuite extends TestConf {
 
         jsonToken = given()
                 .body(bodyUserPassword).contentType(ContentType.JSON)
-        .when()
+                .when()
                 .post(EndPoint.AUTH)
-        .then()
-                .extract().body().asString();
+                .then()
+                .extract().jsonPath().getString("token");
     }
 
     @Test
     public void getBookingIds() {
-        //обязательно ли писать given?
-        when()
+        String expected = "[\n" +
+                "    {\n" +
+                "      \"bookingid\": 1\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"bookingid\": 2\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"bookingid\": 3\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"bookingid\": 4\n" +
+                "    }\n" +
+                "]";
+
+        String result = when()
                 .get(EndPoint.BOOKING)
-        .then().log().all();
+        .then()
+                .extract().body().jsonPath().prettyPrint();
+
+        assertThat(result, equalTo(expected));
     }
+
 
     @Test
     public void getBooking() {
-        given()
+
+        String expected = "{\n" +
+                "    \"firstname\": \"Sally\",\n" +
+                "    \"lastname\": \"Brown\",\n" +
+                "    \"totalprice\": 111,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "        \"checkin\": \"2013-02-23\",\n" +
+                "        \"checkout\": \"2014-10-23\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\": \"Breakfast\"\n" +
+                "}";
+        String result = given()
                 .pathParam("bookingId", 1)
         .when()
                 .get(EndPoint.SINGLE_BOOKING)
         .then()
-                .log().all();
+                .extract().jsonPath().prettyPrint();
+
+        assertThat(result, equalTo(expected));
     }
 
     @Test
@@ -72,68 +105,104 @@ public class TestSuite extends TestConf {
                 "    },\n" +
                 "    \"additionalneeds\" : \"Breakfast\"\n" +
                 "}";
-        given()
+
+        JsonPath jsonPath = given()
                 .body(bodyNewBooking)
         .when()
                 .post(EndPoint.BOOKING)
         .then()
-                .log().all();
+                .extract().jsonPath();
+
+
+        id = jsonPath.getString("bookingid");
+
+        String expected = "{\n" +
+                "    \"bookingid\": " + id + ",\n" +
+                "    \"booking\": {\n" +
+                "        \"firstname\": \"Jim\",\n" +
+                "        \"lastname\": \"Brown\",\n" +
+                "        \"totalprice\": 111,\n" +
+                "        \"depositpaid\": true,\n" +
+                "        \"bookingdates\": {\n" +
+                "            \"checkin\": \"2018-01-01\",\n" +
+                "            \"checkout\": \"2019-01-01\"\n" +
+                "        },\n" +
+                "        \"additionalneeds\": \"Breakfast\"\n" +
+                "    }\n" +
+                "}";
+
+        assertThat(jsonPath.prettify(), equalTo(expected));
     }
 
     @Test
     public void putUpdateBooking() {
 
         String bodyNewBooking = "{\n" +
-                "    \"firstname\" : \"James\",\n" +
-                "    \"lastname\" : \"Brown\",\n" +
-                "    \"totalprice\" : 111,\n" +
-                "    \"depositpaid\" : true,\n" +
-                "    \"bookingdates\" : {\n" +
-                "        \"checkin\" : \"2018-01-01\",\n" +
-                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    \"firstname\": \"James\",\n" +
+                "    \"lastname\": \"Brown\",\n" +
+                "    \"totalprice\": 111,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "        \"checkin\": \"2018-01-01\",\n" +
+                "        \"checkout\": \"2019-01-01\"\n" +
                 "    },\n" +
-                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "    \"additionalneeds\": \"Breakfast\"\n" +
                 "}";
 
-        given()
+        String result = given()
                 .body(bodyNewBooking).pathParam("bookingId", 1)
-//                .contentType(ContentType.JSON).accept(ContentType.JSON)
-                .cookie("token=" + JsonPath.from(jsonToken).getString("token"))
+                .cookie("token", jsonToken)
         .when()
                 .put(EndPoint.SINGLE_BOOKING)
         .then()
-                .log().all();
+                .extract().jsonPath().prettify();
+
+        assertThat(result, equalTo(bodyNewBooking));
     }
 
     @Test
     public void patchUpdateBooking() {
 
-        String bodyNewBooking = "{\n" +
-                "    \"firstname\" : \"James\",\n" +
-                "    \"lastname\" : \"Brown\"\n" +
+        String expected = "{\n" +
+                "    \"firstname\": \"James\",\n" +
+                "    \"lastname\": \"Brown\",\n" +
+                "    \"totalprice\": 111,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "        \"checkin\": \"2018-01-01\",\n" +
+                "        \"checkout\": \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\": \"Breakfast\"\n" +
                 "}";
 
-        given()
+        String bodyNewBooking = "{\n" +
+                "    \"firstname\": \"James\",\n" +
+                "    \"lastname\": \"Brown\"\n" +
+                "}";
+
+        String result = given()
                 .body(bodyNewBooking).pathParam("bookingId", 1)
-//                .contentType(ContentType.JSON).accept(ContentType.JSON)
-                .cookie("token=" + JsonPath.from(jsonToken).getString("token"))
+                .cookie("token", jsonToken)
         .when()
                 .patch(EndPoint.SINGLE_BOOKING)
         .then()
-                .log().all();
+                .extract().jsonPath().prettify();
+
+        assertThat(result, equalTo(expected));
     }
 
     @Test
     public void deleteBooking() {
 
-        given()
-                .cookie("token=" + JsonPath.from(jsonToken).getString("token"))
-                .pathParam("bookingId", 1)
+        String response = given()
+                .cookie("token", jsonToken)
+                .pathParam("bookingId", id)
         .when()
                 .delete(EndPoint.SINGLE_BOOKING)
         .then()
-                .log().all();
+                .extract().response().asString();
+
+        assertThat(response, equalTo("Created"));
     }
-
-
 }
+
